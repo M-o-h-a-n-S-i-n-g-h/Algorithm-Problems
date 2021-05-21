94fe5d63e3bf3845ea729c61fad826d0ed7a9c0c
mohan.singh1385664 <mohan.singh1385664@gmail.com>
2021-05-21 10:06:14 +0530

    Updated

--- Anagram/Anagram.java
+++ Anagram/Anagram.java
@@ -1,32 +1,34 @@
 package Anagram;
-import java.util.*;
 
 public class Anagram {
 
-    static void findAnagram() {
-        String s1 = "geeksforgeeks";
-        String s2 = "mohan";
-        HashMap<Character, Integer> set = new HashMap<>();
-        HashMap<Character, Integer> set1 = new HashMap<>();
-        for(int i = 0; i < s1.length(); i++) {
-            if(!set.containsKey(s1.charAt(i))) {
-                set.put(s1.charAt(i), 1);
-            }else {
-                set.put(s1.charAt(i), set.get(s1.charAt(i)) + 1);
+    static String findAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return "Not an anagram";
+        }
+
+        int[] tracker = new int[26];
+        for (char i : s1.toCharArray()) {
+            tracker[i - 'a']++;
+        }
+
+        for (char i : s2.toCharArray()) {
+            tracker[i - 'a']++;
+        }
+
+        for (char i : s1.toCharArray()) {
+            if ((tracker[i - 'a'] & 1) == 1) {
+                return "Not an anagram";
             }
         }
-        for(int i = 0; i < s2.length(); i++) {
-            if(!set1.containsKey(s2.charAt(i))) {
-                set1.put(s2.charAt(i), 1);
-            }else {
-                set1.put(s2.charAt(i), set1.get(s2.charAt(i)) + 1);
-            }
-        }
-        System.out.println(set);
-        System.out.println(set1);
+        return "Anagram";
     }
 
+
     public static void main(String[] args) {
-        findAnagram();
+        String s1 = "geeksforgeeks";
+        String s2 = "geeksgeeksfor";
+        String result = findAnagram(s1.toLowerCase(), s2.toLowerCase());
+        System.out.println(result);
     }
 }
